88c11a941e073555df99fe2585e878e4d5e07b06
Jessica Benson <jess.nicole.benson@gmail.com>
2023-02-13 23:03:29 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+var trueOrFalse = 'OK for True. CANCEL for False: '
+
+var questionsArr = [
+    { question: trueOrFalse + 'The Pittsburgh Steelers won the Super Bowl six times.',
+    answer: true },
+    { question: trueOrFalse + 'Golden Retrievers are the most popular dog breed in the United States.', 
+    answer: true },
+    { question: trueOrFalse + 'There are 45 states in the United States.', 
+    answer: false },
+    { question: trueOrFalse + 'Tallahassee is the capital of Florida.', 
+    answer: true },
+    { question: trueOrFalse + 'Sharks are mammals.', 
+    answer: false }
+]
